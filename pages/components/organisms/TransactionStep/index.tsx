02ba7e128744775ab56f33d22af40d1d0da5d884
Ghixandra <118959293+ghixandraj@users.noreply.github.com>
2023-05-22 21:01:14 +0700

    index.tsx
    Signed-off-by: Ghixandra <118959293+ghixandraj@users.noreply.github.com>

--- pages/components/organisms/TransactionStep/index.tsx
+++ pages/components/organisms/TransactionStep/index.tsx
@@ -5,10 +5,10 @@ export default function TransactionStep() {
     <section id="feature" className="feature pt-50 pb-50">
       <div className="container-fluid">
         <h2 className="text-4xl fw-bold color-palette-1 text-center mb-30">
-          It’s Really That
+          Be the champion
           <br />
           {' '}
-          Easy to Win the Game
+          Win the game easily
         </h2>
         <div className="row gap-lg-0 gap-4" data-aos="fade-up">
           <StepItem
