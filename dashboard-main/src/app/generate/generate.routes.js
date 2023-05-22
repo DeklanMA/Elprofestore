e00d629ee3f3c638d7ee4b71ca4ca34190f2c773
DeklanMA <93185535+DeklanMA@users.noreply.github.com>
2023-05-22 21:58:19 +0700

    Delete dashboard-main directory
    Signed-off-by: DeklanMA <93185535+DeklanMA@users.noreply.github.com>

--- dashboard-main/src/app/generate/generate.routes.js
+++
@@ -1,9 +0,0 @@
-import { ApiURL } from "../../helpers/index.helper.js";
-import { generateChekcout, generateUsers } from "./generate.controller.js";
-
-function GenerateRoutes(app) {
-  app.route(ApiURL("/generates/users")).post(generateUsers);
-  app.route(ApiURL("/generates/checkout")).post(generateChekcout);
-}
-
-export default GenerateRoutes;
